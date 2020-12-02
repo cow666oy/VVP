2129636c339167fab8011f815670b32632d7bb13
Nikita <nikita-kuznetsov-2018@list.ru>
2020-12-02 17:16:00 +0300

    code

--- лаб 5_1/main.cpp
+++ лаб 5_1/main.cpp
@@ -1,14 +1,9 @@
-//
-//  main.cpp
-//  лаб 5_1
-//
-//  Created by Никита Кузнецов on 02.12.2020.
-//
-
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+    double x1, x2, y1, y2;
+    cin >> x1 >> x2 >> y1 >> y2;
+    double L = sqrt((x2-x1)*(x2-x1)+ (y2-y1)*(y2-y1));
+    cout << L;
 }
